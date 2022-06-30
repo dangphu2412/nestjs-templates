dd6901b7e2927143f7b2be3fd51a5d86e073a34a
jackdang <dangphu241299@gmail.com>
2022-06-30 21:34:04 +0700

    feat: Move jwt feature to global

--- base-project/backend-nestjs/src/authentication/auth.module.ts
+++ base-project/backend-nestjs/src/authentication/auth.module.ts
@@ -3,26 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthServiceToken } from './client/auth.service';
 import { AuthServiceImpl } from './auth.service';
 import { UserModule } from '../user/user.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthorizationModule } from '../authorization/authorization.module';
 
 @Module({
-  imports: [
-    UserModule,
-    AuthorizationModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [UserModule, AuthorizationModule],
   controllers: [AuthController],
   providers: [
-    JwtStrategy,
     {
       provide: AuthServiceToken,
       useClass: AuthServiceImpl,
--- base-project/backend-nestjs/src/shared/shared.module.ts
+++ base-project/backend-nestjs/src/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BcryptService } from './bcrypt.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Global()
 @Module({
@@ -8,8 +10,15 @@ import { BcryptService } from './bcrypt.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
-  providers: [BcryptService, ConfigService],
-  exports: [BcryptService, ConfigService],
+  providers: [BcryptService, ConfigService, JwtStrategy, JwtService],
+  exports: [BcryptService, ConfigService, JwtService],
 })
 export class SharedModule {}
