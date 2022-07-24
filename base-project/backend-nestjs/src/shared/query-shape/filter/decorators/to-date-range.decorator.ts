59295adb7885c20189b7c184fc0697b384d6b8f9
dangphu2412 <dangphu241299@gmail.com>
2022-07-24 20:01:11 +0700

    feat: integrate date range of filter api

---
+++ base-project/backend-nestjs/src/shared/query-shape/filter/decorators/to-date-range.decorator.ts
@@ -0,0 +1,18 @@
+import { Transform, TransformFnParams } from 'class-transformer';
+import { DateRange } from '../entities/date-range';
+import { isEmpty } from 'class-validator';
+
+export function ToDateRange() {
+  const SPLITTER = ',';
+
+  return Transform((params: TransformFnParams) => {
+    if (!params.value) {
+      return undefined;
+    }
+    const [fromDate, toDate] = (params.value as string).split(SPLITTER);
+    const dateRange = new DateRange();
+    dateRange.fromDate = isEmpty(fromDate) ? undefined : new Date(fromDate);
+    dateRange.toDate = isEmpty(toDate) ? undefined : new Date(toDate);
+    return dateRange;
+  });
+}
--- base-project/backend-nestjs/src/user/entities/dtos/get-user-query.dto.ts
+++ base-project/backend-nestjs/src/user/entities/dtos/get-user-query.dto.ts
@@ -1,3 +1,10 @@
 import { OffsetPagination } from '../../../shared/query-shape/pagination/entities/offset-pagination.request';
+import { DateRange } from '../../../shared/query-shape/filter/entities/date-range';
+import { ToDateRange } from '../../../shared/query-shape/filter/decorators/to-date-range.decorator';
+import { ValidateNested } from 'class-validator';
 
-export class GetUserQueryDto extends OffsetPagination {}
+export class GetUserQueryDto extends OffsetPagination {
+  @ToDateRange()
+  @ValidateNested()
+  activeDateRange?: DateRange;
+}
