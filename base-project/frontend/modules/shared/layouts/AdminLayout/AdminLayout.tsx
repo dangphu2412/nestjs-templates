a70cd789796d9eb9622756f9ee4e6e4b8538e956
dangphu2412 <dangphu241299@gmail.com>
2022-07-26 17:47:37 +0700

    feat: finish layout separation

--- base-project/frontend/modules/shared/layouts/AdminLayout/AdminLayout.tsx
+++ base-project/frontend/modules/shared/layouts/AdminLayout/AdminLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Header } from '../../components/Header/Header';
 import { ChildrenPropOnly } from '../../types/react.types';
 import { AuthenticatedGuard } from '../../../auth/components/AuthenticatedGuard/AuthenticatedGuard.component';
@@ -11,8 +12,25 @@ export function AdminLayout({
   return (
     <>
       <AuthenticatedGuard publicRoutes={['/login']} defaultRoute="/">
-        <Header />
-        {children}
+        <Grid
+          h="200px"
+          templateRows="repeat(3, 1fr)"
+          templateColumns="repeat(5, 1fr)"
+          gap={4}
+        >
+          <GridItem rowSpan={3} colSpan={1} bg="tomato">
+            Left Menu
+          </GridItem>
+          <GridItem colSpan={4} bg="papayawhip">
+            <Header />
+          </GridItem>
+          <GridItem colSpan={4} bg="tomato">
+            {children}
+          </GridItem>
+          <GridItem colSpan={4} bg="tomato">
+            Footer
+          </GridItem>
+        </Grid>
       </AuthenticatedGuard>
     </>
   );
