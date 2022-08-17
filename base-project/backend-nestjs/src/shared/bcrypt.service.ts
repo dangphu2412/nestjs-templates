bfb6788cc2f4298e736458b4817836a9e06c0950
dangphu2412 <dangphu241299@gmail.com>
2022-08-17 22:04:14 +0700

    fix: change to bcryptjs for building docker

--- base-project/backend-nestjs/src/shared/bcrypt.service.ts
+++ base-project/backend-nestjs/src/shared/bcrypt.service.ts
@@ -1,4 +1,4 @@
-import { hash, compare, genSalt } from 'bcrypt';
+import { hash, compare, genSalt } from 'bcryptjs';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -10,12 +10,12 @@ export class BcryptService {
     this.saltRounds = parseInt(configService.get<string>('SALT_ROUNDS', '10'));
   }
 
-  public async hash(data: string | Buffer) {
+  public async hash(data: string): Promise<string> {
     const salt = await genSalt(this.saltRounds);
     return hash(data, salt);
   }
 
-  public async compare(data: string, encrypted: string | Buffer) {
+  public async compare(data: string, encrypted: string): Promise<boolean> {
     return compare(encrypted, data);
   }
 }
