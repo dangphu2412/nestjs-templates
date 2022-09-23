612ee91180d85b24c4d01d8da01841413dbad839
dangphu2412 <dangphu241299@gmail.com>
2022-09-23 17:30:42 +0700

    fix: fix gateway

--- base-project/base-microservice/example-gateway/src/app.module.ts
+++ base-project/base-microservice/example-gateway/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { NewsModule } from './news/news.module';
-import { NotificationModule } from './notifications/notification.module';
 
 @Module({
-  imports: [SharedModule, NewsModule, NotificationModule],
+  imports: [SharedModule, NewsModule],
 })
 export class AppModule {}
